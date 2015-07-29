e9a41f5b6a85231bd2557d3204b53a4564812933
Nishil Shah <nishil.shah17@gmail.com>
2015-07-29 18:53:29 -0400

    v 1.1 fantasy draft host

--- FantasyDraftHost/js/index.js
+++ FantasyDraftHost/js/index.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  var uid;
 
   $('#signin').click(function() {
     var ref = new Firebase("https://fantasy-draft-host.firebaseio.com");
@@ -7,6 +8,8 @@ $(document).ready(function() {
         console.log("Login Failed!", error);
       } else {
         console.log("Authenticated successfully with payload:", authData);
+        uid = authData.uid;
+        document.getElementById('#afterAuth').style.zIndex = 1000;
       }
     });
   });
