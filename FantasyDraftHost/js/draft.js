a1a03137e6b592c201d23234af31d4eac845f0df
Nishil Shah <nishil.shah17@gmail.com>
2015-07-30 23:03:41 -0400

    3.9

--- FantasyDraftHost/js/draft.js
+++ FantasyDraftHost/js/draft.js
@@ -43,7 +43,7 @@ $(document).ready(function() {
 
   draftsRef.once('value', function(draftsSnapshot) {
     $('#draftList').empty();
-    var draftListTable = $("<table cellpadding='25'></table>");
+    var draftListTable = $("<table cellpadding='20'></table>");
     draftListTable.attr('id','draftListTable');
     draftListTable.attr('class','flat-table flat-table-3');
     var titleRow = $('<tr><th><b>League</b></th><th><b>Sport</b></th><th><b>Teams</b></th><th><b>Rounds</b></th><th><b>Timer</b></th><th><b>Draft</b></th></tr>');
@@ -72,7 +72,7 @@ $(document).ready(function() {
   });
 
   $('#draftList').on('click','input', function() {
-    document.getElementById('draftList').style.position = "fixed";
+    //document.getElementById('draftList').style.position = "fixed";
     document.getElementById('draftList').style.zIndex = -2125;
 
     draftActive = true;
