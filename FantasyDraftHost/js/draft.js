b484ef6f63314e6027e5639e30dc63839ec1f74e
Nishil Shah <nishil.shah17@gmail.com>
2015-08-04 17:33:33 -0400

    fixed issue where timer up

--- FantasyDraftHost/js/draft.js
+++ FantasyDraftHost/js/draft.js
@@ -219,6 +219,8 @@ $(document).ready(function() {
 
 function initiateCountdown() {
 
+  document.getElementById('timer').style.zIndex = -1000;
+
   countdown = Tock({
       countdown: true,
       interval: 50,
