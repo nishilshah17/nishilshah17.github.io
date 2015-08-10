0d16318b515c05f37a1a67ed407c42b7cc9bfacf
Nishil Shah <nishil.shah17@gmail.com>
2015-08-10 15:34:44 -0400

    fd

--- FantasyDraftHost/js/setup.js
+++ FantasyDraftHost/js/setup.js
@@ -119,6 +119,7 @@ function sendWelcome(to) {
     },
     beforeSend: function(xhr) {
       xhr.setRequestHeader('Authorization',make_base_auth(accountSID,authToken));
+      alert(to);
     },
     success: function(data) {
     }
